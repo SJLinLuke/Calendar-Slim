12ccf2b3f6893ce27ca10e235a4c5dc16a4906d2
LukeLin <luke-lin@mail.sndaits.com>
2025-03-07 17:08:57 +0800

    Initial Commit

---
+++ CalendarView/CalendarViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarViewApp.swift
+//  CalendarView
+//
+//  Created by TWIPC00714163M on 2025/3/7.
+//
+
+import SwiftUI
+
+@main
+struct CalendarViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalendarView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalendarView
+//
+//  Created by TWIPC00714163M on 2025/3/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
